a388f8c38e0fc8af7c3ddc556f4f6787b63ec231
cratas <petr.kratochvil.st1@vsb.cz>
2022-02-03 09:15:22 +0100

    animations detail edited

--- src/components/content/mainContainer/MainContainer.js
+++ src/components/content/mainContainer/MainContainer.js
@@ -12,53 +12,7 @@ function MainContainer() {
         <Container className={classes.container}>
           <NavbarContainer />
           <p>
-            Lionel Andrés Messi Cuccitini[1] (* 24. června 1987 Rosario,
-            Argentina) je argentinský reprezentační fotbalista, hrající za
-            francouzský klub Paris Saint-Germain FC. Je sedminásobným držitelem
-            ceny Zlatý míč (případně Zlatý míč FIFA) a šestinásobným držitelem
-            Zlaté kopačky.[2][3] Rekordně sedmkrát získal cenu Trofeo Pichichi
-            pro nejlepšího střelce španělské ligy.[4] Ve dresu Barcelony získal
-            34 klubových trofejí včetně 10 titulů španělského ligového mistra a
-            čtyř triumfů v Lize mistrů UEFA (platné k únoru 2021).[5] V
-            profesionální kariéře odehrál více než 900 zápasů.[6] V sezóně
-            2011/12 nastřílel ve španělské lize rekordních 50 gólů a ve všech
-            soutěžích též vstřelil rekordních 73 gólů.[7] Vstřelil nejvíce gólů
-            v jednom utkání hlavní fáze Ligy mistrů UEFA, v odvetě osmifinále LM
-            2011/12 odehraného 7. března 2012 proti Bayeru Leverkusen nastřílel
-            5 gólů (Barcelona zvítězila 7:1 a postoupila dál).[8] [p. 1] V
-            listopadu 2014 překonal Raúla v počtu gólů (71) vstřelených v hlavní
-            fázi Ligy mistrů, později byl ale sám překonán svým kariérním
-            rivalem Cristianem Ronaldem, který je první se 134 góly, druhý je
-            právě Messi se 118 góly (platné k únoru 2021).[11][12] V průběhu
-            prosince 2012 překonal po dosažení 86. branky předchozí světový
-            rekord německého útočníka Gerda Müllera z roku 1972 v počtu gólů
-            nastřílených za kalendářní rok a do konce roku tuto porci navýšil na
-            91 branek. Současně Messi 9. prosince 2012 překonal rekord
-            historicky nejlepšího střelce Barcelony Césara Rodrígueze, který v
-            letech 1939–1955 nastřílel 190 gólů. V utkání proti Seville docílil
-            hranici 191 gólů, přičemž někteří statistikové uvedli, že vstřelil
-            za FC Barcelona dokonce 192 branek.[13] Dne 22. listopadu 2014
-            vstřelil hattrick v ligovém zápase proti Sevilla FC, čímž docílil
-            253. branky v Primera División a stal se historicky nejlepším
-            střelcem nejvyšší španělské ligy (překonal 251 gólů Telmo Zarry z
-            roku 1955).[14] Začátkem prosince 2014 překonal hranici 400 gólů v
-            dresu FC Barcelona ve všech zápasech (včetně přípravných).[15] Na
-            konci ledna roku 2021 vstřelil Messi 650. soutěžní gól za
-            Barcelonu.[16] V červnu 2020 vsítil Messi svůj 700. gól
-            profesionální kariéry.[17] V prosinci 2020 překonal Pelého jakožto
-            fotbalista s nejvíce góly za jeden klub.[18] Zároveň je historicky
-            nejlepším střelcem El Clásica (derby proti Realu Madrid, 26 gólů) a
-            Derbi barceloní (derby proti Espanyolu Barcelona, 25 gólů).[19][20]
-            V národním mužstvu pětkrát dosáhl turnajového finále – roku 2014 na
-            Mistrovství světa a v letech 2007, 2015, 2016 a 2021 na Copa América
-            – v prvních čtyřech případech ale odešel jako poražený.[21] V roce
-            2021 zaznamenal první reprezentační úspěch vyjma zlaté medaile z
-            Olympijských her v Pekingu v roce 2021, když Argentina na Copa
-            America ve finále porazila pořadatele Brazílii. Celkově odehrál přes
-            150 reprezentačních zápasů a vstřelil 76 gólů, což jej činí
-            historicky nejlepším střelcem své země.[22][23] Profesionální
-            kariéru měl do roku 2021 celou spojenou se španělským (katalánským)
-            klubem FC Barcelona, kde působil přes 20 let.
+sf
           </p>
         </Container>
         ;
