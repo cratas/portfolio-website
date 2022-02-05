e3ea1a393d7ebd2123c3458b7233700867101b55
cratas <petr.kratochvil.st1@vsb.cz>
2022-02-05 15:53:28 +0100

    navbar desing edited

--- src/components/content/mainContainer/MainContainer.js
+++ src/components/content/mainContainer/MainContainer.js
@@ -11,7 +11,15 @@ function MainContainer() {
       <div className={classes.contentWrapper}>
         <Container className={classes.container}>
           <NavbarContainer />
-          <div className={classes.test}></div>
+          <div className={classes.test}>
+            {/* <h1>Petr Kratochvíl</h1>
+            <h2>Computer science student with passion for web development.</h2>
+            <p>
+            I'am a Computer Science student from Czech Republic with a passion
+            for web development. I'am a Computer Science student from Czech
+            Republic with a passion for web development.
+          </p> */}
+          </div>
         </Container>
       </div>
     </>
