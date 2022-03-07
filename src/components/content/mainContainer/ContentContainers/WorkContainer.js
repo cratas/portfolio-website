8657783a03f689da3fc9c10fb3e114e1a059c7b9
cratas <petr.kratochvil.st1@vsb.cz>
2022-03-07 23:44:43 +0100

    work projects added

--- src/components/content/mainContainer/ContentContainers/WorkContainer.js
+++ src/components/content/mainContainer/ContentContainers/WorkContainer.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-
 import commonClasses from "./CommonContentContainer.module.css";
 import classes from "./WorkContainer.module.css";
+
+import scenePic from './../../../../assets/scene.jpg';
 
 const WorkContainer = () => {
   return (
@@ -11,7 +12,23 @@ const WorkContainer = () => {
         <h2>{"> "}Some of my work.</h2>
       </div>
 
+      <div className={classes.projectWrapper}>
+        <div className={classes.textSite}>
+          <h2>OpenGl game engine</h2>
 
+          <div className={classes.descriptionWrapper}>
+            <p>
+              OpenGl game engine written in C++. The project uses
+              object-oriented programming and together with the technique of
+              design patterns
+            </p>
+          </div>
+        </div>
+
+        <div className={classes.imageSite}>
+            <img src={scenePic} className={classes.profilePicture} alt="Me" sizes='500px'></img>
+        </div>
+      </div>
     </section>
   );
 };
