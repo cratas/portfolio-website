85875d42898d32fca5f9abd4d2ffa26db4aa3699
cratas <petr.kratochvil.st1@vsb.cz>
2022-03-08 00:12:35 +0100

    right side main project example made, but without responsivity

--- src/components/content/mainContainer/ContentContainers/WorkContainer.js
+++ src/components/content/mainContainer/ContentContainers/WorkContainer.js
@@ -3,7 +3,7 @@ import React from "react";
 import commonClasses from "./CommonContentContainer.module.css";
 import classes from "./WorkContainer.module.css";
 
-import scenePic from './../../../../assets/scene.jpg';
+import scenePic from "./../../../../assets/scene.jpg";
 
 const WorkContainer = () => {
   return (
@@ -13,22 +13,37 @@ const WorkContainer = () => {
       </div>
 
       <div className={classes.projectWrapper}>
-        <div className={classes.textSite}>
+        <div className={classes.textSite} data-aos="fade-right">
+          <h5>Recent project</h5>
           <h2>OpenGl game engine</h2>
 
           <div className={classes.descriptionWrapper}>
             <p>
               OpenGl game engine written in C++. The project uses
-              object-oriented programming and together with the technique of
-              design patterns
+              object-oriented programming together with the technique of design
+              patterns
             </p>
+          </div>
+
+          <div className={classes.technologiesWrapper}>
+            <div className={classes.technology}>C++</div>
+            <div className={classes.technology}>OpenGL</div>
+            <div className={classes.technology}>VS code</div>
+            <div className={classes.technology}>OOP</div>
+
           </div>
         </div>
 
-        <div className={classes.imageSite}>
-            <img src={scenePic} className={classes.profilePicture} alt="Me" sizes='500px'></img>
+        <div className={classes.imageSite} data-aos="fade-up">
+          <img
+            src={scenePic}
+            className={classes.profilePicture}
+            alt="Me"
+            sizes="500px"
+          ></img>
         </div>
       </div>
+      
     </section>
   );
 };
